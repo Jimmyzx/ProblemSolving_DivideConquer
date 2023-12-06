963827636b98424fa64daa95bd01252aa21a5d81
Jimmyzx <125935094+Jimmyzx@users.noreply.github.com>
2023-12-06 13:47:31 -0800

    updated notes

--- Coding-problems/JavaScript-Common-Prefix.js
+++ Coding-problems/JavaScript-Common-Prefix.js
@@ -24,35 +24,51 @@
 // loop through words
 // set prefix to matching part of this word to smallest word
 
+
+// Declare an array of strings
 const strings = ["floor", "flour", "flower", "flow"];
+
+// Call the findCommonPrefix function with the array of strings and assign the result to the variable "result"
 const result = findCommonPrefix(strings);
 
+// Declare the findCommonPrefix function, which takes an array of strings as a parameter
 function findCommonPrefix(strings) {
-    if (!strings || strings.length === 0) {
-      return '';
-    }
-  
-    // Sort the array of strings to bring similar prefixes together
-    strings.sort();
-  
-    // Take the first and last strings (sorted) and find the common prefix
-    const firstStr = strings[0];
-    const lastStr = strings[strings.length - 1];
-    let commonPrefix = '';
-  
-    for (let i = 0; i < firstStr.length; i++) {
-      if (firstStr[i] === lastStr[i]) {
-        commonPrefix += firstStr[i];
-      } else {
-        break; // Exit the loop if a mismatch is found
-      }
-    }
-  
-    return commonPrefix;
+  // Check if the input array is null or empty
+  if (!strings || strings.length === 0) {
+    // If so, return an empty string as there is no common prefix
+    return '';
   }
-  
-  console.log(result); // Output: "flo"
-  
+
+  // Sort the array of strings in lexicographical order 
+  // Sort the array of strings to bring similar prefixes together
+  strings.sort();
+
+  // Take the first string (lexicographically smallest) and the last string (lexicographically largest) after sorting
+  const firstStr = strings[0];
+  const lastStr = strings[strings.length - 1];
+
+  // Initialize an empty string to store the common prefix
+  let commonPrefix = '';
+
+  // Iterate through the characters of the first string
+  for (let i = 0; i < firstStr.length; i++) {
+    // Check if the current character at position 'i' is the same in both the first and last strings
+    if (firstStr[i] === lastStr[i]) {
+      // If they are the same, add the character to the common prefix
+      commonPrefix += firstStr[i];
+    } else {
+      // If a mismatch is found, exit the loop
+      break;
+    }
+  }
+
+  // Return the common prefix
+  return commonPrefix;
+}
+
+// Output the result to the console
+console.log(result); // Output: "flo"
+
 
   //This function first sorts the array of strings to ensure that the strings with similar prefixes are adjacent. Then, it compares the characters of the first and last strings (after sorting) until a mismatch is found. The common prefix is built character by character until the mismatch occurs. In the example provided with the array ["floor", "flour", "flower", "flow"], the output will be "flo".
 
